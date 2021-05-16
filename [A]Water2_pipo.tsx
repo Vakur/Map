16d410516a10947c36a4913e68b5e41e46718d35
Vakur <84268708+Vakur@users.noreply.github.com>
2021-05-16 22:01:33 +0100

    Initial commit

---
+++ [A]Water2_pipo.tsx
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="[A]Water_pipo" tilewidth="32" tileheight="32" tilecount="3072" columns="64">
+ <image source="../Tilesets/RGP Tileset/Pipoya RPG Tileset 32x32/SampleMap/[A]Water_pipo.png" width="2048" height="1536"/>
+ <tile id="389">
+  <animation>
+   <frame tileid="437" duration="300"/>
+   <frame tileid="421" duration="300"/>
+   <frame tileid="405" duration="300"/>
+   <frame tileid="389" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="390">
+  <animation>
+   <frame tileid="446" duration="300"/>
+   <frame tileid="430" duration="300"/>
+   <frame tileid="414" duration="300"/>
+   <frame tileid="398" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="391">
+  <animation>
+   <frame tileid="447" duration="300"/>
+   <frame tileid="431" duration="300"/>
+   <frame tileid="415" duration="300"/>
+   <frame tileid="399" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="453">
+  <animation>
+   <frame tileid="509" duration="300"/>
+   <frame tileid="493" duration="300"/>
+   <frame tileid="477" duration="300"/>
+   <frame tileid="461" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="455">
+  <animation>
+   <frame tileid="511" duration="300"/>
+   <frame tileid="495" duration="300"/>
+   <frame tileid="479" duration="300"/>
+   <frame tileid="463" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="517">
+  <animation>
+   <frame tileid="573" duration="300"/>
+   <frame tileid="557" duration="300"/>
+   <frame tileid="541" duration="300"/>
+   <frame tileid="525" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="518">
+  <animation>
+   <frame tileid="574" duration="300"/>
+   <frame tileid="558" duration="300"/>
+   <frame tileid="542" duration="300"/>
+   <frame tileid="526" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="519">
+  <animation>
+   <frame tileid="575" duration="300"/>
+   <frame tileid="559" duration="300"/>
+   <frame tileid="543" duration="300"/>
+   <frame tileid="527" duration="300"/>
+  </animation>
+ </tile>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="Water" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="389" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="390" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="391" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="453" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="455" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="517" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="518" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="519" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="580" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="581" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="644" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="645" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
